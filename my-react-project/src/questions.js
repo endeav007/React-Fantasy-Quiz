6ea42528d3dea8484a3549adedf80173b058c1dc
endeav007 <autumnaustin046@gmail.com>
2025-08-12 11:34:35 -0700

    Finalized Question List

--- my-react-project/src/questions.js
+++ my-react-project/src/questions.js
@@ -2,13 +2,14 @@ export const questions = [
 {
 
 id: 0,
-question: 'Who are you?',
+question: 'This is a losing battle. But is it really?',
 answers: 
 [
-    {personalityType: "A", answer: "Who are you?"},
-    {personalityType: "B", answer: "I'm Me."},
-    {personalityType: "C", answer: "Nobody."},
-    {personalityType: "D", answer: "I don't know."},
+    {personalityType: "Fighter", answer: "Yes. There's no chance at winning this."},
+    {personalityType: "Healer", answer: "I'm still not sure."},
+    {personalityType: "Dreamer", answer: "There has to be another way."},
+    {personalityType: "Archer", answer: "I know it is, but I can't accept it."},
+    {personalityType: "Storyteller", answer: "It is, but that's not going to stop me from trying."}
 
 ]   
 
@@ -17,20 +18,173 @@ answers:
 
 {
     id:1,
-    question: 'What are you',
+    question: 'When do you accept this is a losing battle?',
 
     answers:
     [
-        {personalityType: "A", answer: "A problem?"},
-        {personalityType: "B", answer: "Myself."},
-        {personalityType: "C", answer: "A person."},
-        {personalityType: "D", answer: "I don't know?"},
+        {personalityType: "Figher", answer: "The moment I see what we're up against."},
+        {personalityType: "Healer", answer: "When there's nothing I can do to help."},
+        {personalityType: "Dreamer", answer: "When I run out of ideas."},
+        {personalityType: "Archer", answer: "When I fall."},
+        {personalityType: "SToryteller", answer: "When my strongest attack isn't enough."}
     ]
     
     
 
-}
+}, 
+
+{
+    id:2,
+    question: 'Why are we losing?',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "I wasn't strong enough."},
+        {personalityType: "Healer", answer: "We weren't prepared."},
+        {personalityType: "Dreamer", answer: "We just got unlucky."},
+        {personalityType: "Archer", answer: "We weren't meant to win. It's fate."},
+        {personalityType: "Storyteller", answer: "A single mistake."}
+    ]
+    
+    
+
+}, 
+
+{
+    id:3,
+    question: 'Whose fault is this?',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "My own."},
+        {personalityType: "Healer", answer: "Our own ambition."},
+        {personalityType: "Dreamer", answer: "Whoever placed us on this path."},
+        {personalityType: "Archer", answer: "The villain we’re fighting."},
+        {personalityType: "Storyteller", answer: "Nobody's"}
+    ]
+    
+}, 
+
+
+{
+    id:4,
+    question: 'Was this battle necessary?',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "My own."},
+        {personalityType: "Healer", answer: "There must've been another way. "},
+        {personalityType: "Dreamer", answer: "Whoever placed us on this path."},
+        {personalityType: "Archer", answer: "I was fully against it from the beginning."},
+        {personalityType: "Storyteller", answer: "It was, though I wish it wasn't."}
+    ]
+    
+}, 
+
+{
+    id:5,
+    question: 'What are you leaving behind? ',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "My legacy."},
+        {personalityType: "Healer", answer: "The people I love."},
+        {personalityType: "Dreamer", answer: "My hopes and dreams."},
+        {personalityType: "Archer", answer: "Nothing. Everything I have is right here."},
+        {personalityType: "Storyteller", answer: "Promises. So many promises."}
+    ]
+    
+},
+
+{
+    id:6,
+    question: 'What is your final move? ',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "One final strike."},
+        {personalityType: "Healer", answer: "I give my allies the rest of my strength."},
+        {personalityType: "Dreamer", answer: "I look at my team for the last time."},
+        {personalityType: "Archer", answer: "I search for any way out."},
+        {personalityType: "Storyteller", answer: "I think about the path it took to get here."}
+    ]
+    
+}, 
+
+{
+    id:7,
+    question: 'This is the last time you’ll fight alongside these people. When did you meet them? ',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "When I needed something to fight for."},
+        {personalityType: "Healer", answer: "When I was looking for help."},
+        {personalityType: "Dreamer", answer: "When I was at my lowest. "},
+        {personalityType: "Archer", answer: "When I decided to be better."},
+        {personalityType: "Storyteller", answer: "When I was searching for something new. "}
+    ]
+    
+}, 
+
+{
+    id:8,
+    question: 'This is the last time you will fight alongside these people. When did you meet them? ',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "When I needed something to fight for."},
+        {personalityType: "Healer", answer: "When I was looking for help."},
+        {personalityType: "Dreamer", answer: "When I was at my lowest. "},
+        {personalityType: "Archer", answer: "When I decided to be better."},
+        {personalityType: "Storyteller", answer: "When I was searching for something new. "}
+    ]
+    
+}, 
+
+{
+    id:9,
+    question: 'This is the last time you will fight alongside these people. What is your favorite memory of them?',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "The time we tried a ridiculous plan, and it worked out perfectly. "},
+        {personalityType: "Healer", answer: "The time they stayed by my side when I was unwell."},
+        {personalityType: "Dreamer", answer: "The time we celebrated our first victory. "},
+        {personalityType: "Archer", answer: "The time I could finally share my story with them."},
+        {personalityType: "Storyteller", answer: "The time I first saw them. "}
+    ]
+    
+}, 
+
+{
+    id:9,
+    question: 'This is the last time you will fight alongside these people. What do you say to them?',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "Thank you for everything."},
+        {personalityType: "Healer", answer: "I hope we can meet again someday. "},
+        {personalityType: "Dreamer", answer: "This better not be the end. "},
+        {personalityType: "Archer", answer: "Goodbye friends. "},
+        {personalityType: "Storyteller", answer: "I love you all."}
+    ]
+    
+}, 
+
+{
+    id:10,
+    question: 'It is the end. What do you think about?',
+
+    answers:
+    [
+        {personalityType: "Fighter", answer: "If there's any way I can keep fighting."},
+        {personalityType: "Healer", answer: "What this means for the world."},
+        {personalityType: "Dreamer", answer: "Everyone I've ever loved or lost."},
+        {personalityType: "Archer", answer: "About all that was left to say."},
+        {personalityType: "Storyteller", answer: "What led up to this point."}
+    ]
+    
+}, 
 
 ]
-
 
